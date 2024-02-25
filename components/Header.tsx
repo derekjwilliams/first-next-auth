3c360a0b92c3bff8fc9f37a0a3b0ab57cef37ee0
derek williams <derek61@gmail.com>
2024-02-25 16:29:55 -0700

    removed attribution

--- components/Header.tsx
+++ components/Header.tsx
@@ -23,12 +23,6 @@ export default function Header() {
         </a>
       </div>
       <h1 className="sr-only">Supabase and Next.js Starter Template</h1>
-      <p className="attribution italic text-xs">
-        <a className="h-2" target="_blank" rel="noopener noreferrer" href="https://www.flickr.com/photos/153584064@N07/50640508551">Marigold, Calendula (1596-1610) by Anselmus Boëtius de Boodt. Original from the Rijksmuseum. Digitally enhanced by rawpixel.</a>
-        by <a target="_blank" rel="noopener noreferrer" href="https://www.flickr.com/photos/153584064@N07">Free Public Domain Illustrations by rawpixel</a> 
-         is licensed under <a target="_blank" rel="noopener noreferrer" href="https://creativecommons.org/licenses/by/2.0/?ref=openverse">CC BY 2.0 <img className="inline h-4" src="https://mirrors.creativecommons.org/presskit/icons/cc.svg"></img><img src="https://mirrors.creativecommons.org/presskit/icons/by.svg" className="inline h-4"></img></a>
-
-        </p>
     </div>
   );
 }
