638a6f726f0169b561ef0f9d79f1fd13da09d9fd
Jason Redcedar <jasonredcedar@gmail.com>
2024-02-25 15:33:43 -0800

    change by line

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,8 +4,8 @@ import SupabaseLogo from "./SupabaseLogo";
 export default function Header() {
   return (
     <div className="flex flex-col gap-16 items-center">
-      <p className="text-3xl lg:text-4xl !leading-tight mx-auto max-w-xl text-center">
-        Creating collaborative relationships between property owners and renters
+      <p className="text-2xl lg:text-2xl !leading-tight mx-auto max-w-xl text-center">
+        Creating collaborative relationships between property owners and tenants.
       </p>
       <img src="/marigold_logo.png"/>
       <div className="flex gap-8 justify-center items-center">
