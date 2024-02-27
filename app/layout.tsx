f39aac545f37e3491cfb2bf74630e0ace3ce8e77
derek williams <derek61@gmail.com>
2024-02-27 15:53:47 -0700

    update to title and description

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Marigold",
+  description: "Creating collaborative relationships between property owners and tenants",
 };
 
 export default function RootLayout({
