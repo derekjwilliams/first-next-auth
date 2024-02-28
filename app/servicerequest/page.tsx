d36721175c30ab530ed2136a454e4be30b18313b
derek williams <derek61@gmail.com>
2024-02-28 10:38:28 -0700

    draft version of service request page

---
+++ app/servicerequest/page.tsx
@@ -0,0 +1,87 @@
+import AuthButton from "@/components/AuthButton";
+import { createClient } from "@/utils/supabase/server";
+import Header from "@/components/Header";
+import { redirect } from "next/navigation";
+
+export default async function ProtectedPage() {
+  const supabase = createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    return redirect("/login");
+  }
+
+  return (
+
+
+    
+    <div className="flex-1 w-full flex flex-col gap-20 items-center">
+      <div className="w-full">
+        <div className="py-6 font-bold bg-flower_red text-center text-white">
+          This is a protected page that you can only see as an authenticated
+          user
+        </div>
+        <nav className="w-full flex justify-end border-b border-b-foreground/10 h-16">
+          <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
+            <AuthButton />
+          </div>
+        </nav>
+      </div>
+
+      <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 font-figtree text-2xl">
+        <main className="flex-1 flex flex-col gap-6">
+        <div className="grid-even-columns">
+            <div className="service-item shadow-md bg-flower_gold">
+                Safety
+            </div>
+            <div className="service-item shadow-md bg-flower_gold">
+                Heating and Cooling
+            </div>
+            <div className="service-item shadow-md bg-flower_gold">
+                Pests
+            </div>
+            <div className="service-item shadow-md bg-flower_gold">
+                Wall, Floors, Doors, Windows
+            </div>
+            <div className="service-item shadow-md bg-flower_gold">
+                Electrical
+            </div>
+            <div className="service-item shadow-md bg-flower_gold">
+                Broadband
+            </div>
+            <div className="service-item shadow-md bg-flower_gold">
+                Laundry
+            </div>
+            <div className="service-item shadow-md bg-flower_gold">
+                Door and Lock
+            </div>
+            <div className="service-item shadow-md bg-flower_gold">
+                Garbage Disposal
+            </div>
+            <div className="service-item shadow-md bg-flower_gold">
+                Faucet
+            </div>
+            <div className="service-item shadow-md bg-flower_gold">
+                Drain
+            </div>
+            <div className="service-item shadow-md bg-flower_gold">
+                Roof
+            </div>
+            <div className="service-item shadow-md  bg-flower_gold">
+                Gutters
+            </div>
+            <div className="service-item shadow-md bg-flower_gold">
+                Trees, Lawn, Landscaping
+            </div>
+          </div>
+        </main>
+      </div>
+
+      <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
+      </footer>
+    </div>
+  );
+}
