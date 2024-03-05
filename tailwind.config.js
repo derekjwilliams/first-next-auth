36fd843545e2c3275c25e3a0a052c62acc086f29
derek williams <derek61@gmail.com>
2024-03-05 14:12:05 -0700

    add tailwind config back

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        figtree: ['Figtree', 'sans-serif'],
+      },
+      colors: {
+        flower_yellow: '#FFD55F',
+        flower_gold: '#FFC200',
+        flower_red: '#F74600',
+        environment_brown: '#D67A00',
+        leaf: '#005145',
+        pansy: '#A800AD',
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+        btn: {
+          background: 'hsl(var(--btn-background))',
+          'background-hover': 'hsl(var(--btn-background-hover))',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
