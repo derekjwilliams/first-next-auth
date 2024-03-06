fea9ccb03251fafb37877d612cfcea6a6f3b8299
derek williams <derek61@gmail.com>
2024-03-06 16:44:29 -0700

    updated linting

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-uses-react': 'off',
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+}
--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -31,8 +31,6 @@ export default async function ProtectedPage() {
       <div className="animate-in flex-1 flex flex-col gap-20 max-w-4xl px-3">
         <Header />
         <main className="flex-1 flex flex-col gap-6">
-          {/* <h2 className="font-bold text-4xl mb-4">Next steps</h2>
-          <FetchDataSteps /> */}
         </main>
       </div>
 
