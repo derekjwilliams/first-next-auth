836826fcaa19df0ca21a69f34ca4a3317a5f149b
derek williams <derek61@gmail.com>
2024-03-06 14:51:11 -0700

    added styleX variables

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,5 @@
 import * as stylex from "@stylexjs/stylex";
-import { colors } from "@stylexjs/open-props/lib/colors.stylex";
+import { marigoldColors } from '../app/marigoldColors.stylex'
 
 const header_swatch = stylex.create({
     base: {
@@ -7,22 +7,22 @@ const header_swatch = stylex.create({
         minHeight: '4rem',
     },
     flower_yellow: {
-      backgroundColor: 'rgb(255 213 95)'
+      backgroundColor: marigoldColors.flowerYellow,
     },
     flower_gold: {
-      backgroundColor: 'rgb(255 194 0)'
+      backgroundColor: marigoldColors.flowerGold,
     },
     flower_red: {
-      backgroundColor: 'rgb(247 70 0)'
+      backgroundColor: marigoldColors.flowerRed
     },
     environment_brown: {
-      backgroundColor: 'rgb(214 122 0)'
+      backgroundColor: marigoldColors.environmentBrown
     },
     leaf: {
-      backgroundColor: 'rgb(0 81 69)'
+      backgroundColor: marigoldColors.leaf
     },
     pansy: {
-      backgroundColor: 'rgb(168 0 173)'
+      backgroundColor: marigoldColors.pansy
     }
 })
 export default function Header() {
@@ -41,16 +41,6 @@ export default function Header() {
         Creating collaborative relationships between property owners and tenants.
       </p>
       <img src="/marigold_logo.png"/>
-      <div className="flex gap-8 justify-center items-center">
-        <a
-          href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-          target="_blank"
-          rel="noreferrer"
-        >
-        </a>
-        <span className="border-l rotate-45 h-6" />
-      </div>
-      <h1 className="sr-only">Supabase and Next.js Starter Template</h1>
     </div>
   );
 }
