ecda645dbda2e571b263ac84097c67118b4cde14
derek williams <derek61@gmail.com>
2024-03-19 17:24:30 -0600

    update toast message

--- components/ReactQueryClientProvider.tsx
+++ components/ReactQueryClientProvider.tsx
@@ -7,17 +7,19 @@ import {
   QueryClientProvider,
 } from '@tanstack/react-query'
 type MyErrorResponse = {
-  message: string
   code: string
+  details: string
   hint: string
+  message: string
 }
 
 const queryClient = new QueryClient({
   queryCache: new QueryCache({
     onError: (error) => {
+      console.log(JSON.stringify(error))
       let e: MyErrorResponse = JSON.parse(JSON.stringify(error)) // workaround for Tanstack Query error type issue
       toast.error(
-        `Something went wrong: ${e.message}.  ${
+        `Something went wrong: ${error.message}.  ${
           e.code === 'PGRST116'
             ? 'This can be caused by trying to load a protected page'
             : ''
