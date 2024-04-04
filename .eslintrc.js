3b9af44aa70ac61bcacd14c63ac2ac44906fbfa0
derek williams <derek61@gmail.com>
2024-04-04 11:38:53 -0600

    added tanstack eslint plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,12 @@
 module.exports = {
   extends: 'next/core-web-vitals',
-  plugins: ['@stylexjs'],
+  plugins: ['@stylexjs', '@tanstack/query'],
   rules: {
     '@stylexjs/valid-styles': 'error',
     'no-color-literals': 'off',
     'jsx-quotes': ['error', 'prefer-single'],
+    '@tanstack/query/exhaustive-deps': 'error',
+    '@tanstack/query/no-rest-destructuring': 'warn',
+    '@tanstack/query/stable-query-client': 'error',
   },
 }
