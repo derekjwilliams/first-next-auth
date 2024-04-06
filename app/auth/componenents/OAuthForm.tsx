8a4442f9b81bd8ca7a0a86e6d934eea8189c95d7
derek williams <derek61@gmail.com>
2024-04-06 17:50:49 -0600

    Added OAuthForm

---
+++ app/auth/componenents/OAuthForm.tsx
@@ -0,0 +1,36 @@
+'use client'
+import { createBrowserClient } from '@supabase/ssr'
+import React from 'react'
+
+export default function OAuthForm() {
+  const supabase = createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  )
+  const loginWithGithub = () => {
+    supabase.auth.signInWithOAuth({
+      provider: 'github',
+      options: {
+        redirectTo: `${location.origin}/auth-server-action/callback`,
+      },
+    })
+  }
+  const loginWithGoogle = () => {
+    supabase.auth.signInWithOAuth({
+      provider: 'google',
+      options: {
+        redirectTo: `${location.origin}/auth-server-action/callback`,
+      },
+    })
+  }
+  return (
+    <>
+      <button className='w-full' onClick={loginWithGithub}>
+        Login With Github
+      </button>
+      <button className='w-full' onClick={loginWithGoogle}>
+        Login With Google
+      </button>
+    </>
+  )
+}
---
+++ app/login/callback/route.ts
@@ -0,0 +1,52 @@
+import { cookies } from 'next/headers'
+import { NextResponse } from 'next/server'
+import { type CookieOptions, createServerClient } from '@supabase/ssr'
+
+export async function GET(request: Request) {
+  // from  https://youtu.be/PdmKlne1gRY?si=A3J-MZvqwV3Oge06&t=1026
+  // not sure if needed, tbd
+
+  const requestUrl = new URL(request.url)
+
+  const { searchParams } = requestUrl
+
+  //   const { searchParams, origin } = new URL(request.url)
+  const code = searchParams.get('code')
+  // if "next" is in param, use it as the redirect URL
+  const next = searchParams.get('next') ?? '/'
+
+  // NOTE, completed code in the demo branch has the functionality in
+  // app/oauth/callback/route.ts not
+  // app/auth-server-action/callback/route.ts
+  // code is also different, see: https://github.com/Chensokheng/next-14-supabase-ssr/blob/demo/app/oauth/callback/route.ts
+  if (code) {
+    const cookieStore = cookies()
+    const supabase = createServerClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+      {
+        cookies: {
+          get(name: string) {
+            return cookieStore.get(name)?.value
+          },
+          set(name: string, value: string, options: CookieOptions) {
+            cookieStore.set({ name, value, ...options })
+          },
+          remove(name: string, options: CookieOptions) {
+            cookieStore.delete({ name, ...options })
+          },
+        },
+      }
+    )
+
+    const { error } = await supabase.auth.exchangeCodeForSession(code)
+    if (!error) {
+      return NextResponse.redirect(`${requestUrl.origin}/servicerequests`) // from , at https://youtu.be/PdmKlne1gRY?si=A3J-MZvqwV3Oge06&t=1026
+      //return NextResponse.redirect(`${origin}${next}`) original
+    }
+  }
+
+  // return the user to an error page with instructions
+  // TODO handle error
+  //return NextResponse.redirect(`${origin}/auth/auth-code-error`)
+}
