83d609b5f5e9afd5136ece645ddee55122583439
derek williams <derek61@gmail.com>
2024-05-25 13:06:25 -0600

    create service request styling

--- src/components/SafetyServiceRequests.tsx
+++ src/components/SafetyServiceRequests.tsx
@@ -65,7 +65,7 @@ const form = stylex.create({
     cursor: 'pointer',
     textDecoration: 'none',
     color: 'black',
-    fontSize: fonts.size4,
+    fontSize: fonts.size2,
     boxShadow: 'rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.1) 0px 4px 6px -4px',
     borderRadius: '0.5rem',
     placeItems: 'center',
