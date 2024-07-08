0dea221100de24919e75cac01c93a002ad2d1de7
derek williams <derek61@gmail.com>
2024-07-07 18:27:34 -0600

    removed compiler from eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   extends: 'next/core-web-vitals',
   plugins: ['eslint-plugin-react-compiler', '@stylexjs', '@tanstack/query'],
   rules: {
-    'react-compiler/react-compiler': 'error',
+    // 'react-compiler/react-compiler': 'error',
     '@stylexjs/valid-styles': 'error',
     'no-color-literals': 'off',
     'jsx-quotes': ['error', 'prefer-single'],
