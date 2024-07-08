2734b9442badbbc4e500336e010e48c8717532e8
derek williams <derek61@gmail.com>
2024-07-07 19:47:27 -0600

    blocknotes added

--- src/app/demotailwind/page.tsx
+++ src/app/demotailwind/page.tsx
@@ -1,25 +1,14 @@
 'use client'
 
-import './layout.css'
 import dynamic from 'next/dynamic'
 import { useState, useMemo } from 'react'
 import TextareaAutosize from 'react-textarea-autosize'
 
-export default function page() {
-  const Editor = useMemo(() => dynamic(() => import('./Editor'), { ssr: false }), [])
+export default function Home() {
+  const Editor = useMemo(() => dynamic(() => import('@/components/Editor'), { ssr: false }), [])
   return (
-    <Editor onChange={() => {}} />
-    // <main className='min-h-screen'>
-    //   <div className='flex flex-col px-24 py-10 w-full'>
-    //     <div className='group flex flex-col gap-2'>
-    //       <TextareaAutosize
-    //         placeholder='Untitled'
-    //         className='w-full resize-none appearance-none overflow-hidden bg-transparent text-5xl font-bold focus:outline-none'
-    //       />
-    //     </div>
-    //     <Editor onChange={() => {}} />
-    //   </div>
-    // </main>
+    <main className='min-h-screen'>
+      <Editor />
+    </main>
   )
-  // return <h1 className='text-3xl font-bold underline bg-slate-50'>Hello tailwind!</h1>
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/app/demotailwind/**/*.{js,ts,jsx,tsx,mdx}'],
+  content: ['./src/app/demotailwind/**/*.{js,ts,jsx,tsx,mdx}', './src/components/Editor.tsx'],
   theme: {
     extend: {},
   },
