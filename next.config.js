6fb3b8aed7f4cee925cbfb0a0e9320a1c870b766
derek williams <derek61@gmail.com>
2024-07-07 18:29:47 -0600

    removed compiler from next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 stylexPlugin = require('@stylexjs/nextjs-plugin')
 const nextConfig = {
-  experimental: {
-    reactCompiler: false,
-  },
   reactStrictMode: true,
   optimizeFonts: false,
   pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
