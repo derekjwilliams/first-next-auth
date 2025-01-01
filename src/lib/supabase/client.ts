a0cc410a51c359e2968f0fe51fc2d8d2045c2d12
derek williams <derek61@gmail.com>
2025-01-01 15:13:41 -0700

    removed unused css, removed demo lexical being used for development

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -1,18 +1,18 @@
 import { createServerClient, type CookieOptions } from '@supabase/ssr'
 import { UnsafeUnwrappedCookies, cookies } from 'next/headers'
 
-// TODO check latest supabase examples, should we have const cookieStore = await cookies()?
+// TODO check latest supabase examples, createServerClient and UnsafeUnwrappedCookies are deprecated now
 export const createClient = () => {
   const cookieStore = cookies() as unknown as UnsafeUnwrappedCookies
   return createServerClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!, {
     cookies: {
       async get(name: string) {
-        return await cookieStore.get(name)?.value
+        return (await cookieStore).get(name)?.value
       },
       async set(name: string, value: string, options: CookieOptions) {
         options.sameSite = 'lax'
         try {
-          cookieStore.set({ name, value, ...options })
+          ;(await cookieStore).set({ name, value, ...options })
         } catch (error) {
           // The `set` method was called from a Server Component.
           // This can be ignored if you have middleware refreshing
