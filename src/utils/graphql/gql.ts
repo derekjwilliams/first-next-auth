a0bb70a8205d4dd3b242b562c01a24f1b8e0c173
derek williams <derek61@gmail.com>
2025-01-06 16:47:32 -0700

    remove problematic fragment-masking code

--- src/utils/graphql/gql.ts
+++ src/utils/graphql/gql.ts
@@ -1,9 +1,5 @@
 /* eslint-disable */
-import * as types from './graphql';
-
-
-
 const documents = {}
 export function graphql(source: string) {
-  return (documents as any)[source] ?? {};
+  return (documents as any)[source] ?? {}
 }
