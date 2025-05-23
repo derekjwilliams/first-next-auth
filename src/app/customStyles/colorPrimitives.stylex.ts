a7fe7cdce4e4d3b60a7f67e02b06add2e60f30a3
derek williams <derek61@gmail.com>
2025-05-23 12:04:09 -0600

    added color primitives

---
+++ src/app/customStyles/colorPrimitives.stylex.ts
@@ -0,0 +1,47 @@
+import * as stylex from '@stylexjs/stylex'
+export const colorPrimitives = stylex.defineVars({
+  // Brights
+  marigoldPetal: 'rgb(255, 153, 18)',
+  marigoldGold: 'rgb(255, 194, 0)',
+  marigoldAmber: 'rgb(232, 104, 14)',
+  marigoldRust: 'rgb(200, 60, 0)',
+  marigoldRed: 'rgb(247, 70, 0)',
+
+  // Darks
+  marigoldCenter: 'rgb(168, 80, 0)',
+  marigoldBrown: 'rgb(214, 122, 0)',
+  marigoldLeaf: 'rgb(0, 81, 69)',
+  marigoldLeafLight: 'rgb(189, 219, 163)',
+  marigoldStem: 'rgb(44, 120, 60)',
+  marigoldSlate: 'rgb(68, 68, 68)',
+
+  // Neutrals
+  marigoldSky: 'rgb(120, 180, 235)',
+  marigoldCloud: 'rgb(240, 245, 250)',
+  marigoldPath: 'rgb(130, 130, 130)',
+  marigoldStoneLight: '#F7F8FA',
+  marigoldStone: '#ECEFF1',
+  marigoldStoneMid: '#D3D6DA',
+  marigoldStoneDark: '#A0A4AA',
+  marigoldStoneDarker: '#6B6E74',
+
+  // Dark mode/neutral
+  marigoldEggplant: 'rgb(36, 36, 40)',
+  marigoldNight: 'rgb(24, 24, 28)',
+  marigoldMist: 'rgb(235, 230, 255)',
+  marigoldFog: 'rgb(180, 180, 200)',
+
+  // Desaturated versions (approximate, 30-40% less saturation)
+  marigoldPetalDesaturated: 'rgb(224, 170, 97)',
+  marigoldGoldDesaturated: 'rgb(224, 200, 97)',
+  marigoldAmberDesaturated: 'rgb(200, 140, 97)',
+  marigoldRustDesaturated: 'rgb(180, 110, 80)',
+  marigoldRedDesaturated: 'rgb(210, 120, 97)',
+
+  marigoldCenterDesaturated: 'rgb(150, 110, 80)',
+  marigoldBrownDesaturated: 'rgb(180, 140, 97)',
+  marigoldLeafDesaturated: 'rgb(60, 100, 90)',
+  marigoldLeafLightDesaturated: 'rgb(170, 200, 170)',
+  marigoldStemDesaturated: 'rgb(90, 130, 100)',
+  marigoldSlateDesaturated: 'rgb(100, 100, 100)',
+})
